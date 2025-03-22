5b869307edde8011ff88a081268056b3d4b72289
Akashdeep2696 <akashdeep2696@gmail.com>
2025-03-23 00:49:57 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,17 +1,23 @@
 import psutil
 import time
+import os
 
-while True:
-        cpu_usage = psutil.cpu_percent(interval=1)
-        memory_usage = psutil.virtual_memory().percent
-        disk_usage = psutil.disk_usage('/').percent
+THRESHOLD = 75
+def monitor_resources():
+        while True:
+                cpu_usage = psutil.cpu_percent(interval=1)
+                memory_usage = psutil.virtual_memory().percent
+                disk_usage = psutil.disk_usage('/').percent
 
-        print(f"CPU Usage: {cpu_usage}%")
-        print(f"MEMORY Usage: {memory_usage}%")
-        print(f"DISK Usage: {disk_usage}%")
+                print(f"CPU Usage: {cpu_usage}%")
+                print(f"MEMORY Usage: {memory_usage}%")
+                print(f"DISK Usage: {disk_usage}%")
 
-        if cpu_usage > 75 or memory_usage > 75 or disk_usage > 75:
+                if cpu_usage > THRESHOLD or memory_usage > THRESHOLD or disk_usage > THRESHOLD:
 
-                print("Resource usage exceed 75%")
+                        print("Resource usage exceeds the threshold. Triggering scale-up process.")
+                        os.system("gcloud compute instance-groups managed resize cloud-instance-group --size=2 --zone=us-central1-a")
 
-        time.sleep(10)
+                time.sleep(5)
+                
+monitor_resources()
